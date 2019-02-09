980b37ac3397545bf218ad817abab02b821f30e2
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2019-02-09 20:00:12 +0900

    success_jobcan_login

--- selenium.rb
+++ selenium.rb
@@ -1,20 +1,29 @@
-# API
-# https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings
-
 require 'selenium-webdriver'
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 driver = Selenium::WebDriver.for :chrome, options: options
 
-# mail_adress = ENV['MYJOBCAN_MAIL_ADDRESS']
-# login_password = ENV['MYJOBCAN_PASSWORD']
+# sec
+# wait = Selenium::WebDriver::Wait.new(timeout: 10)
+# # wait.until {}
+
+# inputで数を受けられるようにしたい。決まった数を解釈して一気に入力したい
 
 driver.navigate.to "https://id.jobcan.jp/users/sign_in"
 
-p "resized"
+mail_adress = ENV['MYJOBCAN_MAIL_ADDRESS']
+login_password = ENV['MYJOBCAN_PASSWORD']
+driver.find_element(:id, "user_email").send_keys(mail_adress)
+driver.find_element(:id, "user_password").send_keys(login_password)
+driver.find_element(:xpath, "//*[@id='new_user']/input[3]").click
+
+p "login successfully"
+
 driver.manage.window.resize_to(800, 800)
+
+p "resized"
+driver.save_screenshot "sample.png"
 p "take screenshot"
-driver.save_screenshot "intoli-screenshot.png"
 
 driver.quit
