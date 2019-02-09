f5d6884cb95308b9ec6ec752a5112ba8e37d51a4
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2019-02-09 15:44:04 +0900

    add_headlesschrome

--- selenium.rb
+++ selenium.rb
@@ -1,14 +1,20 @@
+# API
+# https://github.com/SeleniumHQ/selenium/wiki/Ruby-Bindings
+
 require 'selenium-webdriver'
 
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to "https://jobcan.ne.jp/"
-driver.quit
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')
+driver = Selenium::WebDriver.for :chrome, options: options
 
-# driver.navigate.to "http://google.com"
-# element = driver.find_element(name: 'q')
-# element.send_keys "Hello WebDriver!"
-# element.submit
-#
-# puts driver.title
-#
-# driver.quit
+# mail_adress = ENV['MYJOBCAN_MAIL_ADDRESS']
+# login_password = ENV['MYJOBCAN_PASSWORD']
+
+driver.navigate.to "https://id.jobcan.jp/users/sign_in"
+
+p "resized"
+driver.manage.window.resize_to(800, 800)
+p "take screenshot"
+driver.save_screenshot "intoli-screenshot.png"
+
+driver.quit
