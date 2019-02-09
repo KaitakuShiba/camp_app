5e5574970c20497494df2f92ac9f0dc9b408367b
KaitakuShiba <roadbiker.kaitaku@gmail.com>
2019-02-10 05:43:00 +0900

    success_inputs_but_no_save

--- selenium.rb
+++ selenium.rb
@@ -1,12 +1,11 @@
 require 'selenium-webdriver'
+require 'date'
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
 options.add_argument("--user-data-dir=" + ENV['YOUR_PROFILE_PATH'])
 driver = Selenium::WebDriver.for :chrome, options: options
 wait = Selenium::WebDriver::Wait.new(timeout: 10)
-
-# inputで数を受けられるようにしたい。決まった数を解釈して一気に入力したい
 
 driver.navigate.to "https://id.jobcan.jp/users/sign_in"
 mail_adress = ENV['MYJOBCAN_MAIL_ADDRESS']
@@ -31,20 +30,63 @@ driver.find_element(:xpath, "//*[@id='menu_man_hour_manage']/table/tbody/tr[1]/t
 
 p "Moved Man-hour management screen .."
 
-driver.save_screenshot "aaa.png"
-p "take screenshot"
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+driver.find_elements(:class, "today-record")[0].find_elements(:tag_name, "td")[3].find_elements(:class, "btn")[0].click
 
-driver.manage.delete_all_cookies
+wait.until {
+  driver.find_element(:id, "edit-menu-contents").displayed?
+}
+
+p "Entering parameters .."
+
+driver.find_elements(:class, "btn-default")[0].click
+
+select_project = Selenium::WebDriver::Support::Select.new(driver.find_elements(:name, 'projects[]')[0])
+select_project.options.each do |option|
+  p option.text
+end
+p '+++++++++++++++++++++++++'
+p 'What is your projects?'
+p '+++++++++++++++++++++++++'
+# gets
+select_project.select_by(:text, '共通')
+
+select_task = Selenium::WebDriver::Support::Select.new(driver.find_elements(:name, 'tasks[]')[0])
+select_task.options.each do |option|
+  p option.text
+end
+p '+++++++++++++++++++++++++'
+p 'What is your task?'
+p '+++++++++++++++++++++++++'
+# gets
+select_task.select_by(:text, '保守・機能維持(04)  D0040007')
+
+driver.find_elements(:name, "minutes[]")[0].clear
+p '+++++++++++++++++++++++++'
+p 'What is your ?'
+p '+++++++++++++++++++++++++'
+# gets
+driver.find_elements(:name, "minutes[]")[0].send_keys('10:00')
+
+driver.find_element(:id, "save").location_once_scrolled_into_view
+p '+++++++++++++++++++++++++'
+p 'Time ?'
+p '+++++++++++++++++++++++++'
+# gets
+
+
+driver.find_element(:id, "save").click
+
+# wait.until {
+#   driver.find_element(:id, "edit-menu-contents").length === 0
+# }
+
+# driver.sleep(5000)
+
+driver.save_screenshot "result.png"
+
+# driver.manage.delete_all_cookies
 
 driver.quit
 
-
-# driver.getSessionId().then(function (session) {
-#   console.log(session)
-# })
-
-# driver.manage.add_cookie(name: 'key', value: 'value')
-#
-# driver.manage.all_cookies.each { |cookie|
-#   puts "#{cookie[:name]} => #{cookie[:value]}"
-# }
+p "Save inputs"
